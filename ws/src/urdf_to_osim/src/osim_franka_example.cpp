ad837efd0047bc07200c0c225f283408e0801097
Martin <martinandrovich@gmail.com>
2020-05-14 17:34:35 +0200

    add actuators

--- ws/src/urdf_to_osim/src/osim_franka_example.cpp
+++ ws/src/urdf_to_osim/src/osim_franka_example.cpp
@@ -16,34 +16,22 @@ main(int argc, char** argv)
 	using namespace SimTK;
 	using namespace OpenSim;
 
-	// deduce directories
-	auto dir_pkg  = ros::package::getPath("urdf_to_osim") + "/";
-	auto dir_data = dir_pkg + "data/";
-	auto dir_osim = dir_data + "osim/";
-
-	// check if necessary directories exist
-	if (not boost::filesystem::is_directory(dir_osim))
-	{
-		ROS_WARN("The directory '%s' does not exist; exiting!", dir_osim.c_str());
-		return 0;
-	}
-
 	// ----------------------------------------------------------------------------------------------
 
 	// create OSIM model
 	OpenSim::Model model;
-	auto model_name = "panda";
-	model.setAuthors("Martin Androvich");
+	std::string model_name = "panda";
 	model.setName(model_name);
+	model.setAuthors("Martin Androvich");
 
 	// ----------------------------------------------------------------------------------------------
 
 	// > links
 
-	OpenSim::Body* link0  = new OpenSim::Body("link0", 2.92, Vec3(-0.025566, -2.88e-05, 0.057332), Inertia(0));
-	OpenSim::Body* link1  = new OpenSim::Body("link1", 2.74, Vec3(0, -0.0324958, -0.0675818), Inertia(0));
-	OpenSim::Body* link2  = new OpenSim::Body("link2", 2.74, Vec3(0, -0.06861, 0.0322285), Inertia(0));
-	OpenSim::Body* link3  = new OpenSim::Body("link3", 2.38, Vec3(0.0469893, 0.0316374, -0.031704), Inertia(0));
+	OpenSim::Body* link0  = new OpenSim::Body("link0", 1.92, Vec3(-0.025566, -2.88e-05, 0.057332), Inertia(0));
+	OpenSim::Body* link1  = new OpenSim::Body("link1", 4.970684, Vec3(0, -0.0324958, -0.0675818), Inertia(0.551121276649726, 0.554308164573063, 0.009020836222789, -9.891702555950000e-05, 0.003401254912500, 0.017358801154300));
+	OpenSim::Body* link2  = new OpenSim::Body("link2", 0.646926, Vec3(0, -0.06861, 0.0322285), Inertia(0));
+	OpenSim::Body* link3  = new OpenSim::Body("link3", 3.228604, Vec3(0.0469893, 0.0316374, -0.031704), Inertia(0));
 
 	// ----------------------------------------------------------------------------------------------
 
@@ -108,26 +96,51 @@ main(int argc, char** argv)
 
 	// > contact geometry
 
-	// contact parameters
-	double stiffness = 1E7, dissipation = 0.1, fric_static = 0.6, fric_dynamic = 0.4, viscosity = 0.01;
-	auto contact_params = new OpenSim::HuntCrossleyForce::ContactParameters(stiffness, dissipation, fric_static, fric_dynamic, viscosity);
+	// for the joints, BushingForce should be used to define damping
+	// https://simtk-confluence.stanford.edu:8443/display/OpenSim33/OpenSim+Models#OpenSimModels-Forces
 
-	// link0 contact mesh/sphere
-	ContactSphere* link0_contact = new ContactSphere(0.15, Vec3(0), *link0, "link0_contact");
-	// ContactMesh* link0_contact = new ContactMesh("meshes/collision/link0.stl", Vec3(0), Vec3(0), *link0, "link0_contact");
+	// contact parameters
+	double stiffness = 1e7, dissipation = 0.1, fric_static = 1.05, fric_dynamic = 1.4, fric_viscous = 1.0;
+	auto contact_params = new OpenSim::ElasticFoundationForce::ContactParameters(stiffness, dissipation, fric_static, fric_dynamic, fric_viscous);
+
+	// default contact geometry appearance
+	auto contact_appearance = new OpenSim::Appearance();
+	contact_appearance->set_visible(false);
+
+	// link0 contact mesh
+	auto link0_contact = new ContactMesh("meshes/collision/link0.stl", Vec3(0), Vec3(0), *link0, "link0_contact");
 	model.addContactGeometry(link0_contact);
 
-	// link3 contact sphere
-	ContactSphere* link3_contact = new ContactSphere(0.10, Vec3(0), *link3, "link3_contact");
-	// ContactMesh* link3_contact = new ContactMesh("meshes/collision/link3.stl", Vec3(0), Vec3(0), *link3, "link3_contact");
+	// link1 contact mesh
+	auto link1_contact = new ContactMesh("meshes/collision/link1.stl", Vec3(0), Vec3(0), *link1, "link1_contact");
+	model.addContactGeometry(link1_contact);
+
+	// link2 contact mesh
+	// auto link2_contact = new ContactMesh("meshes/collision/link2.stl", Vec3(0), Vec3(0), *link2, "link2_contact");
+	// model.addContactGeometry(link2_contact);
+
+	// link3 contact mesh
+	auto link3_contact = new ContactMesh("meshes/collision/link3.stl", Vec3(0), Vec3(0), *link3, "link3_contact");
 	model.addContactGeometry(link3_contact);
 
-	// add contact force
-	auto contact_force = new OpenSim::HuntCrossleyForce(contact_params);
+	// add contact forces
+	auto contact_force = new OpenSim::ElasticFoundationForce(contact_params);
 	contact_force->setName("contact_force");
+
 	contact_force->addGeometry("link0_contact");
+	contact_force->addGeometry("link1_contact");
+	// contact_force->addGeometry("link2_contact");
 	contact_force->addGeometry("link3_contact");
+
 	model.addForce(contact_force);
+
+	// ----------------------------------------------------------------------------------------------
+
+	// > actuators
+	
+	// joint0 actuator
+	auto joint0_actuator = new OpenSim::CoordinateActuator(joint0->getCoordinate().getName());
+	model.addModelComponent(joint0_actuator);
 
 	// ----------------------------------------------------------------------------------------------
 
@@ -149,7 +162,7 @@ main(int argc, char** argv)
 	model.finalizeConnections();
 
 	// export model
-	auto path_model = dir_osim + model_name + ".osim";
+	auto path_model = boost::filesystem::current_path().string() + "/" + model_name + ".osim";
 	ROS_INFO("Exporting model to: '%s'", path_model.c_str());
 	model.print(path_model);
 	
