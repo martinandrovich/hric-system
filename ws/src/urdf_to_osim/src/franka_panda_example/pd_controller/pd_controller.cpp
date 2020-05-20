b31cec4c43f9ae28ed44925dd01d6b674b9e0f5e
Martin Androvich <martinandrovich@gmail.com>
2020-05-20 18:43:28 +0200

    made PD controller set desired torques + add OpenSim DLL

--- ws/src/urdf_to_osim/src/franka_panda_example/pd_controller/pd_controller.cpp
+++ ws/src/urdf_to_osim/src/franka_panda_example/pd_controller/pd_controller.cpp
@@ -1,7 +1,51 @@
 #include "pd_controller.h"
 
-int
-main()
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+#include <simbody/internal/SimbodyMatterSubsystem.h>
+
+void
+OpenSim::PDController::computeControls(const SimTK::State& state, SimTK::Vector& vec_controls) const
 {
-	return 0;
-};
+	// > control loop
+	// this method is called at every time step for every actuator
+
+	// get current time in simulation
+	auto t = state.getTime();
+
+	// get joint actuators
+	// lambda to access joints actuators, e.g. q(0) -> joint0
+	auto q = [&](size_t i) { return (const OpenSim::CoordinateActuator&)(this->getActuatorSet()[i]); };
+	//auto q0 = (const OpenSim::CoordinateActuator&)(this->getActuatorSet()[0]);
+
+	// vector of desied torques
+	std::vector<double> vec_tau_des = { 10.0 };
+
+	// assert that number of desired torques = num joints
+	if (vec_tau_des.size() != vec_controls.size())
+	{
+		std::cerr << "Size of desired torques [" << vec_tau_des.size() << "] and number of controls [" << vec_controls.size() << "] does not match!\n";
+		return;
+	}
+
+	// get vector of joint positions
+	auto vec_q = state.getQ();
+	std::cout << "q: " << vec_q << "\n";
+
+	// calculate mass matrix
+	auto& sys = this->getModel().getMultibodySystem().getMatterSubsystem();
+	SimTK::Matrix mat;
+	sys.calcM(state, mat);
+	//std::cout << "sys.getNumBodies(): " << sys.getNumBodies() << std::endl;
+	//std::cout << mat << "\n";
+
+	// apply desired torques
+	SimTK::Vector des_torque(1, 0.0);
+	for (size_t i = 0; i < vec_tau_des.size(); ++i)
+	{
+		des_torque[0] = vec_tau_des[i];
+		q(i).addInControls(des_torque, vec_controls);
+	}
+}
--- ws/src/urdf_to_osim/src/franka_panda_example/pd_controller/pd_controller.h
+++ ws/src/urdf_to_osim/src/franka_panda_example/pd_controller/pd_controller.h
@@ -1,37 +1,26 @@
 #pragma once
 
-#include <OpenSim/OpenSim.h>
 #include <OpenSim/Simulation/Control/Controller.h>
+#include <OpenSim/Actuators/CoordinateActuator.h>
+#include <OpenSim/Simulation/Model/Model.h>
+#include <OpenSim/Simulation/Model/ModelComponent.h>
+
+#ifdef _WIN32
+	#include "osimPluginDLL.h"
+#endif
+
+namespace OpenSim {
 
 class PDController : public OpenSim::Controller {
 OpenSim_DECLARE_CONCRETE_OBJECT(PDController, Controller);
 
 public:
 
+	PDController() {}
 	PDController(double kp, double kd) : Controller(), kp(kp), kd(kd) {}
 
 	void
-	computeControls(const SimTK::State& state, SimTK::Vector &vec_controls) const override
-	{
-		// > control loop
-		// this method is called at every time step for every actuator
-
-		// current time in simulation
-		auto t = state.getTime();
-
-		// get joint actuators
-		// lambda to access joints, e.g. q(0) -> joint0
-		// auto q = [&](size_t i){ return (const OpenSim::CoordinateActuator&)(this->getActuatorSet()[i]); };
-
-		auto q0 = (const OpenSim::CoordinateActuator&)(this->getActuatorSet()[0]);
-
-		// vector of desied torques
-		std::vector<double> tau_des = { 0.0, 0.0 };
-
-		SimTK::Vector vec_desired(1, 10.0);
-		q0.addInControls(vec_desired, vec_controls);
-
-	}
+	computeControls(const SimTK::State& state, SimTK::Vector &vec_controls) const override;
 
 private:
 
@@ -39,3 +28,5 @@ private:
 	double kd;
 
 };
+
+}
