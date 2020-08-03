60f3623622dee517339c47b10071a70be4a9c6e9
Martin Androvich <martinandrovich@gmail.com>
2020-08-03 20:35:19 +0200

    update assets and add final thesis

--- assets/matlab/plot_sim_vs_real.m
+++ assets/matlab/plot_sim_vs_real.m
@@ -12,6 +12,8 @@ max_len = min(length(data_sim), length(data_irl));
 data_sim(max_len + 1:end, :) = [];
 data_irl(max_len + 1:end, :) = [];
 
+%%
+
 % plot joint information for q5
 
 % q5: position
@@ -99,6 +101,29 @@ plot(data_irl(:, 1), data_irl(:, 8))
 set(gcf, 'PaperPosition', [0 0 10 8]);
 set(gcf, 'PaperSize', [10 8]);
 saveas(gcf, "../media/img/sim-vs-real-pos", "pdf");
+
+%%
+
+figure()
+hold on, box on
+plot(data_sim(:, 1), abs(data_irl(:, 2) - data_sim(:, 2)))
+plot(data_sim(:, 1), abs(data_irl(:, 3) - data_sim(:, 3)))
+plot(data_sim(:, 1), abs(data_irl(:, 4) - data_sim(:, 4)))
+plot(data_sim(:, 1), abs(data_irl(:, 5) - data_sim(:, 5)))
+plot(data_sim(:, 1), abs(data_irl(:, 6) - data_sim(:, 6)))
+plot(data_sim(:, 1), abs(data_irl(:, 7) - data_sim(:, 7)))
+plot(data_sim(:, 1), abs(data_irl(:, 8) - data_sim(:, 8)))
+title("Absolute Error in Joint Position (Simulated vs. Real)")
+legend("Joint 1", "Joint 2", "Joint 3", "Joint 4", "Joint 5", "Joint 6", "Joint 7")
+xlabel("Time [s]")
+ylabel("Error [rad]")
+
+set(gcf, 'PaperPosition', [0 0 5 3]);
+set(gcf, 'PaperSize', [5 3]);
+saveas(gcf, "../media/img/sim-vs-real-pos-error", "pdf");
+
+
+%%
 
 function save_figure(filename)
     
